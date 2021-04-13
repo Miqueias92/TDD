b164ca6abb91f5ce4b224bc40e4bff7dd7121dea
Miqueias dos Santos Sousa Assad <misousa@padtec.com.br>
2021-04-12 21:36:42 -0300

    enviando arquivos

---
+++ pilhaTDD/src/TestePilha.java
@@ -0,0 +1,63 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestePilha {
+	
+	public Pilha p;
+	
+	public TestePilha() {
+	
+	}
+	
+	@Before
+	public void inicializaPilha() {
+		p = new Pilha(10);
+	}
+	
+	@Test
+	public void pilhaVazia() {
+		assertTrue(p.estaVazia());
+		assertEquals(0, p.tamanho());
+	}
+	
+	@Test
+	public void empilhaUmElemento() {	
+		p.empilha("primeiro");
+		assertFalse(p.estaVazia());
+		assertEquals(1, p.tamanho());
+		assertEquals("primeiro", p.topo());
+	}
+	
+	@Test
+	public void empilhaUmDesempiha() {
+		p.empilha("primeiro");
+		p.empilha("segundo");
+		assertEquals(2, p.tamanho());
+		assertEquals("segundo", p.topo());
+		Object desempilhado = p.desempilha();
+		assertEquals(1, p.tamanho());
+		assertEquals("primeiro", p.topo());
+		assertEquals("segundo", desempilhado);
+	}
+	
+	@Test(expected = PilhaVaziaException.class)
+	public void removeDaPilha() {
+		p.desempilha();
+	}
+	
+	@Test
+	public void adicionaNaPilhaCheia() {
+		for (int i = 0; i < 10; i++) {
+			p.empilha("elemento" + i);
+		}
+		
+		try {
+			p.empilha("boom");
+			fail();
+		} catch (PilhaCheiaException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ pilhaTDD/src/pilhaTDD/tradutor/TradutorTeste.java
@@ -0,0 +1,64 @@
+package pilhaTDD.tradutor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TradutorTeste {
+
+	private Tradutor tradutor;
+	
+	@Before
+	public void criarTradutor() {
+		tradutor = new Tradutor();
+	}
+	
+	@Test
+	public void tradutorSemPalavras() {
+		assertTrue(tradutor.estaVazio());
+	}
+	
+	@Test
+	public void tradutorUmaPalavra() {
+		tradutor.adicionaTraducao("bom", "good");
+		
+		assertFalse(tradutor.estaVazio());
+		assertEquals("good", tradutor.traduzir("bom"));
+	}
+	
+	@Test
+	public void tradutorDuasPalavras() {
+		tradutor.adicionaTraducao("bom", "good");
+		tradutor.adicionaTraducao("mau", "bad");
+		
+		assertEquals("good", tradutor.traduzir("bom"));
+		assertEquals("bad", tradutor.traduzir("mau"));
+	}
+	
+	@Test
+	public void tradutorDuasTraducoesMesmaPalavras() {
+		tradutor.adicionaTraducao("bom", "good");
+		tradutor.adicionaTraducao("bom", "nice");
+		assertEquals("good, nice", tradutor.traduzir("bom"));
+	}
+	
+	@Test
+	public void tradutorFrase() {
+		tradutor.adicionaTraducao("guerra", "war");
+		tradutor.adicionaTraducao("é", "is");
+		tradutor.adicionaTraducao("ruim", "bad");
+		
+		assertEquals("war is bad", tradutor.traduzirFrase("guerra é ruim"));
+	}
+	
+	@Test
+	public void tradutorFraseComDuasTraducoesMesmaPalavra() {
+		tradutor.adicionaTraducao("paz", "peace");
+		tradutor.adicionaTraducao("é", "is");
+		tradutor.adicionaTraducao("bom", "good");
+		tradutor.adicionaTraducao("bom", "nice");
+		
+		assertEquals("peace is good", tradutor.traduzirFrase("paz é bom"));
+	}
+}
